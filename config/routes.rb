71d0723103d0808eb5f396c461e2fa719ff1d2bd
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-01-02 23:24:49 -0500

    Naked domain redirection setup

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  match "(*any)",
+    to: redirect(subdomain: ""),
+    via: :all,
+    constraints: { subdomain: "www" }
 end
